4779db56082cbe2a35588d3dd0557a1c785b0e4b
Sun/chems Zghaidi <154335653+Chems05@users.noreply.github.com>
2024-09-20 12:51:05 +0200

    Initial commit

---
+++ src/main/java/chems/project/l5_s7/ProjectL5S7Application.java
@@ -0,0 +1,13 @@
+package chems.project.l5_s7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectL5S7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectL5S7Application.class, args);
+	}
+
+}
---
+++ src/test/java/chems/project/l5_s7/ProjectL5S7ApplicationTests.java
@@ -0,0 +1,13 @@
+package chems.project.l5_s7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectL5S7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
